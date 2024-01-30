2b4b8ce7a7b5795b229316d7e86917d86761f96d
David Cardaci <dpcardaci@yahoo.com>
2024-01-30 18:17:12 +0000

    mod

--- ProcessCleanFile.cs
+++ ProcessCleanFile.cs
@@ -215,6 +215,7 @@ namespace FileTransferService.Functions
                 {
                     TransferId = Guid.Parse(metadata["transferid"]),
                     OriginatingUserPrincipalName = metadata["userprincipalname"],
+                    OnBehalfOfUserPrincipalName = metadata"onbehalfofuserprincipalname"
                     OriginationDateTime = DateTime.Parse(metadata["originationdatetime"]),
                     FileName = destBlobName,
                     Message = ex.Message
